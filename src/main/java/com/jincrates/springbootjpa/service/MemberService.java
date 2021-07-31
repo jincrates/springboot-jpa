7e76f680870e74e0b51abfe3ba62fc78e8150cf1
jincrates <94jingyu@gmail.com>
2021-07-31 15:10:08 +0900

    회원 서비스 개발

---
+++ src/main/java/com/jincrates/springbootjpa/service/MemberService.java
@@ -0,0 +1,56 @@
+package com.jincrates.springbootjpa.service;
+
+import com.jincrates.springbootjpa.domain.Member;
+import com.jincrates.springbootjpa.repository.MemberRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true) //javax가 아닌 springframework 사용 권장  //데이터 읽기에 성능 최적화 도움을 줌
+//@RequiredArgsConstructor  //lombok을 이용한 생성자 생성
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * 회원가입
+     */
+    @Transactional // 데이터 쓰기: readOnly = false
+    public Long join(Member member){
+        validateDuplicateMember(member); //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if(!findMembers.isEmpty()) {
+            //EXCEPTION
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    /**
+     * 회원 전체 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    /**
+     * 회원 조회
+     */
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
