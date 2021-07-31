6750100b4e42aeca0050781e5d5fb55bf7cab7dd
jincrates <94jingyu@gmail.com>
2021-07-31 16:20:53 +0900

    상품 리포지토리, 서비스 개발

--- src/main/java/com/jincrates/springbootjpa/domain/item/Item.java
+++ src/main/java/com/jincrates/springbootjpa/domain/item/Item.java
@@ -1,6 +1,7 @@
 package com.jincrates.springbootjpa.domain.item;
 
 import com.jincrates.springbootjpa.domain.Category;
+import com.jincrates.springbootjpa.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,4 +25,25 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직==//
+    // @Setter를 가지고 데이터를 넣고 빼고 하는 것이 아니라, 증감 메서드를 사용
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity -= quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+
+        this.stockQuantity = restStock;
+    }
 }
---
+++ src/main/java/com/jincrates/springbootjpa/service/ItemService.java
@@ -0,0 +1,30 @@
+package com.jincrates.springbootjpa.service;
+
+import com.jincrates.springbootjpa.domain.item.Item;
+import com.jincrates.springbootjpa.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+}
