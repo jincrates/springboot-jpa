bb0f4acb5b525464e6003bd233315137f2aad011
jincrates <94jingyu@gmail.com>
2021-08-03 23:29:48 +0900

    상품 수정

--- src/main/java/com/jincrates/springbootjpa/controller/MemberController.java
+++ src/main/java/com/jincrates/springbootjpa/controller/MemberController.java
@@ -2,12 +2,14 @@ package com.jincrates.springbootjpa.controller;
 
 import com.jincrates.springbootjpa.domain.Address;
 import com.jincrates.springbootjpa.domain.Member;
+import com.jincrates.springbootjpa.domain.item.Book;
 import com.jincrates.springbootjpa.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
